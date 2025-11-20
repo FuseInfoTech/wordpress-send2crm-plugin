2919b318a940a40b3a15e81605f744d1d7368a73
Chris Tyler <ctyler@fuseit.com>
2025-11-21 12:43:34 +1300

    DEV-370 Add plugin bootstrap and autoloader from boilerplate for namespacing

--- send2crm/send2crm.php
+++ send2crm/send2crm.php
@@ -1,29 +1,98 @@
 <?php
 /**
+* This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ * 
+ * @package   Send2CRM Wordpress Plugin
+ * @author    FuseIT  support@fuseit.com
+ * @copyright 2025 Fuse Information Technologies Ltd
+ * @license   GPL v2 or later
+ * @link      https://fuseit.com
+ * 
  * Plugin Name: Send2CRM
- * Description: Unlocking the potential in every sales opportunity.
- *              Seamlessly connects all your websites with your CRM.
- * Version: 0.0.1
+* Plugin URI:      @TODO
+ * Description:     @TODO
+ * Version: 1.0.0
  * Author: FuseIT
  * Author URI: https://fuseit.com
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Requires PHP:    8.3
  */
  
+// In strict mode, only a variable of exact type of the type declaration will be accepted.
+declare(strict_types=1);
+namespace Send2CRM;
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) exit;
+
+// Autoloader
+require_once plugin_dir_path(__FILE__) . 'Autoloader.php';
+
+/**
+ * Current plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('SEND2CRM_VERSION', '1.0.0');
+
+/* The string used to uniquely identify this plugin.
+ */
+define('SEND2CRM_SLUG', 'send2crm');
 
 class Send2CRM {
+    /**
+     * The unique identifier of this plugin.
+     *
+     * @since    1.0.0
+     */
+    protected string $slug;
 
+    /**
+     * The current version of the plugin.
+     *
+     * @since    1.0.0
+     */
+    protected string $version;
+
+    /**
+     * Define the core functionality of the plugin.
+     *
+     * Set the plugin name and the plugin version that can be used throughout the plugin.
+     * Load the dependencies, and set the hooks for the admin area and
+     * the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
     public function __construct() {
+        $this->version = SEND2CRM_VERSION;
+        $this->slug = SEND2CRM_SLUG;
+        error_log('Initializing Send2CRM Plugin'); //TODO Remove Debug statements
+
+        $this->defineHooks();
+
+    }
+
+        /**
+     * Create the objects and register all the hooks of the plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function defineHooks(): void
+    {
+        $isAdmin = is_admin();
+
         error_log('Initializing Send2CRM Plugin');
         // Hook into admin_init 
         add_action('admin_init', array($this,'send2crm_settings_init'));
         // Hook into admin_menu to add our page 
         add_action('admin_menu', array($this,'send2crm_settings_menu'));
         //Hook Send2CRM snippet as script tag in header of public site only and not admin pages
-
-
         add_action('wp_head', array($this,'send2crm_insert_snippet'));
-
     }
 
     public function send2crm_settings_init() {
